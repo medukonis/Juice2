b6d0e863d038ee7bb2c5627dc334737c4a04d0c9
Michael Edukonis <36677289+medukonis@users.noreply.github.com>
2019-06-19 19:19:30 -0400

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -17,17 +17,29 @@ class ViewController: UIViewController
     var right: String!
     var kwh: String!
     var measurements = [String]()
+    var watts: String!
     
     @IBOutlet weak var leftSide: UITextField!
     @IBOutlet weak var rightSide: UITextField!
     @IBOutlet weak var killoWattHours: UITextField!
     @IBOutlet weak var lblStartDate: UILabel!
     @IBOutlet weak var lblStartTime: UILabel!
+    @IBOutlet weak var lblWatts: UILabel!
+    @IBOutlet weak var lblAmps: UILabel!
+    @IBOutlet weak var lblBill: UILabel!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        leftSide.text = "--"
+        rightSide.text = "--"
+        killoWattHours.text = "--"
+        lblWatts.text = "--"
+        lblAmps.text = "--"
+        lblStartDate.text = "--"
+        lblStartTime.text = "--"
+        lblBill.text = "--"
         
     }
 
@@ -42,6 +54,12 @@ class ViewController: UIViewController
         leftSide.text = "--"
         rightSide.text = "--"
         killoWattHours.text = "--"
+        lblWatts.text = "--"
+        lblAmps.text = "--"
+        lblStartDate.text = "--"
+        lblStartTime.text = "--"
+        lblBill.text = "--"
+        
     }
     
     @objc func getTheGoods()
@@ -50,7 +68,7 @@ class ViewController: UIViewController
         
             do
             {
-                var contents = try String(contentsOf:downloadURL, encoding: .utf8)
+                let contents = try String(contentsOf:downloadURL, encoding: .utf8)
                 measurements = contents.components(separatedBy: ",")
                 startDate = measurements[0]
                 startTime = measurements[1]
@@ -63,7 +81,16 @@ class ViewController: UIViewController
                 lblStartDate.text = startDate
                 lblStartTime.text = startTime
                 
+                let l = Double(left)
+                let r = Double(right)
+                let w = Int((l! + r!) * 120)  //watts
+                lblAmps.text =  String(format: "%.2f", (l! + r!))
+                lblWatts.text = String(w)
+                let subst = kwh!.dropLast()
+                let dblkwh = Double(subst)
+                lblBill.text = "$" + String(format: "%.2f", (dblkwh! * 0.1059385))
             }
+                
             catch let error as NSError
             {
                 print("Error: \(error)")
